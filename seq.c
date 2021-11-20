ce9375e4cfcc73204452e34b9ef89eae87dec8f2
Manuel Valenzuela <manuel.valenzuela@protonmail.com>
2021-11-20 15:29:21 -0800

    Added C implementation (almost 4 times faster) and added more results in the results file

---
+++ seq.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  srand(time(NULL));
+  char characters[] = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789";
+  char realWord[] = "monke";
+  int wordLength = strlen(realWord);
+  int totalCharacters = strlen(characters);
+
+  char result[wordLength];
+
+  int counter = 0;
+
+  clock_t start, stop;
+  start = clock();
+
+  while (1) {
+    result[wordLength] = '\0'; // clear the array 
+
+    for (int i = 0; i < wordLength; i++) {
+      result[i] = characters[rand() % totalCharacters];  
+    }
+
+    if (counter % 1000000 == 0) {
+      printf("attempt #%d: %s\n", counter, result);
+    }
+
+    if (strcmp(result, realWord) == 0) {
+      printf("the password is: %s in %d attempts\n", result, counter);
+      break;
+    }
+
+    counter++;
+  }
+  stop = clock();
+
+  printf("Time elapsed: %fs\n", ((double)(stop-start)) / CLOCKS_PER_SEC);
+
+  return 0;
+}
