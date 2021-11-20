42606d8323a6c95d66e7a9066bf86693b2519f39
Manuel Valenzuela <manuel.valenzuela@protonmail.com>
2021-11-20 09:59:17 -0800

    Finished sequential implementation

--- seq.cpp
+++ seq.cpp
@@ -1,24 +1,46 @@
 // #include "omp.h"
 #include <iostream>
+#include <string>
+#include <chrono>
 
 using namespace std;
 
 int main() {
-  string wordToMatch = "monke";
   srand(time(NULL)); 
-  int wordLength = wordToMatch.length();
 
   string characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789";
-  string result = "";
+  string realWord = "sad";
+  string result;
+
+  int wordLength = realWord.length();
 
   bool match = false;
+  int counter = 0;
+  
+  chrono::time_point<chrono::system_clock> start, stop;
+
+  start = chrono::system_clock::now();
 
   while (!match) {
-    match = true;
+    result = "";
     for (int i = 0; i < wordLength; i++) {
-      result[i] = characters[rand() % characters.length()];  
+      result += characters[rand() % characters.length()];  
     }
+
+    if (counter % 1000000 == 0) {
+      printf("attempt #%d: %s\n", counter, result.c_str());
+    }
+
+    if (result == realWord) {
+      printf("the password is: %s in %d attempts\n", result.c_str(), counter);
+      match = true;
+    }
+
+    counter++;
   }
+  stop = chrono::system_clock::now();
+  chrono::duration<double> duration = stop - start;
+  cout << "Time elapsed: " << duration.count() << "s" << endl;
 
   return 0;
 }
